b67d4764429854d337bda63f83ec89a6203ed38c
chrissycotton16 <36048285+chrissycotton16@users.noreply.github.com>
2018-02-06 13:13:02 -0500

    Footer

---
+++ js/footer.js
@@ -0,0 +1,5 @@
+
+$(document).ready(function getFooter() {
+    document.getElementById("footer").innerHTML="Give us feedback by sending us an email to <a href=\"mailto:c.n.cotton@eagle.clarion.edu\">c.n.cotton@eagle.clarion.edu</a>";
+}
+);
