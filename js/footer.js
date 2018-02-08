0c2525fa07bdf623ff4459e03054122956df87fb
cfrye2913 <32493117+cfrye2913@users.noreply.github.com>
2018-02-08 13:14:41 -0500

    Added common footer

--- js/footer.js
+++
@@ -1,5 +0,0 @@
-
-$(document).ready(function getFooter() {
-    document.getElementById("footer").innerHTML="Give us feedback by sending us an email to <a href=\"mailto:c.n.cotton@eagle.clarion.edu\">c.n.cotton@eagle.clarion.edu</a>";
-}
-);
