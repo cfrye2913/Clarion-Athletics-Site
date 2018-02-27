37430c9980fba065373abd5e932226537af3c601
cfrye2913 <32493117+cfrye2913@users.noreply.github.com>
2018-02-27 13:56:57 -0500

    Added Image Uploading

--- pages/admin.php
+++ pages/admin.php
@@ -1,26 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
-
-
-    <title>Clarion University Athletics</title>
-
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="../css/bootstrap.css">
-    <link rel="stylesheet" href="../css/custom.css">
-    <!-- Custom styles for this template -->
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-
-</head>
-
-<body>
-
-<?php include("../includes/navbar.html"); ?>
+<?php
+$title = "Admin Page - Clarion Athletics Site";
+include("../includes/navbar.html"); ?>
 
 <div class="container">
 
@@ -33,8 +13,9 @@
               class = "row pt-5">
             <div class = "col-6 text-right">Select an image to add to the home page:</div>
             <div class = "col-6 text-left pl-3"><input name = "userFile" type = "file"/> </div>
-            <input type = "submit" value = "Upload Image" class = "btn-primary ml-auto mr-auto mt-3"/>
+            <input type = "submit" value = "Upload Image" class = "btn-primary ml-auto mr-auto mt-3 mb-3"/>
         </form>
+        <p class="text-danger">For best quality, images should be 1719 x 967 pixels</p>
     </div>
 </div><!-- /.container -->
 
--- phpFunctionality/addCarouselImages.php
+++ phpFunctionality/addCarouselImages.php
@@ -1,3 +1,19 @@
 <?php
+    //get the file image and store the path that we want to store it at
+    $uploadFile = "../Images/" . $_FILES['userFile']['name'];
 
+    //if the file already exists, we will replace it
+    if(file_exists($uploadFile)){
+        $message = "A file with this name already exists. This file was replaced.";
+    }
+    else{
+        $message = "Image was uploaded successfully";
+    }
+
+    //get image width and height
+    //check to make sure image width and height is similar to size
+    //used in carousel
+    $imageInfo = getimagesize($_FILES['userFile']['tmp_name']);
+    $imageWidth = $imageInfo[0];
+    &imageHeight = $imageInfo[1];
 ?>
