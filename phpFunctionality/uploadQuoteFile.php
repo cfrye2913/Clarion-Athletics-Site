69ebe0432a38d7a750934cdaba7b85fbbefc7db2
ARCooper <mr.aaroncooper@gmail.com>
2018-03-04 15:42:02 -0500

    Added text file uploading
    Administrators are now able to upload  text files. If a file is uploaded into the text file field that is not a file, the user will receive an error message.

--- phpFunctionality/uploadQuoteFile.php
+++ phpFunctionality/uploadQuoteFile.php
@@ -8,10 +8,20 @@
 <h1>Upload a New Quotes File</h1>
 
 <?php
-    $targetdir = "/files";
-    $targetfile = $targetdir . basename($_FILES['userfile']['name']);
-    if(file_exists($targetfile))
+    $targetdir = "../files/";
+    $targetfile = $targetdir . $_FILES['userFile']['name'];
+    if (@mime_content_type($_FILES['userFile']['type']) != "text/html")
     {
-        move_uploaded_file($_FILES['userFile']['name'], $targetfile);
+        $message = 'Incorrect file type.';
     }
+    elseif(file_exists($targetfile))
+    {
+        $message = 'A file with this name already existed. It was replaced.';
+        move_uploaded_file($_FILES['userFile']['tmp_name'], $targetfile);
+    }
+    else {
+        $message = 'File uploaded successfully';
+        move_uploaded_file($_FILES['userFile']['tmp_name'], $targetfile);
+    }
+    echo $message;
 ?>
