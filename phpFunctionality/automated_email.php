56e8f42c0c338afad9496445983c48c21edbf1a4
aaronrcooper <mr.aaroncooper@gmail.com>
2018-03-05 10:45:38 -0500

    Email functionality completed
    Emailing functionality has been tested and works properly. The password has been redacted from the relevant file, however, the functionality works properly.

---
+++ phpFunctionality/automated_email.php
@@ -0,0 +1,31 @@
+<?php
+    //Gets the mail script
+    require_once "Mail.php";
+    //Sender
+    $from="cubeckerlab@gmail.com";
+    //Recipient
+    $to="mr.aaroncooper@gmail.com";
+    //subject
+    $subject = "Test";
+    //content
+    $body = "Hello. \nThis is a new line.";
+
+    //SMTP server name, port, user password
+    $smtpinfo["host"] = "ssl://smtp.gmail.com";
+    $smtpinfo["port"] ="465";
+    $smtpinfo["auth"] = true;
+    $smtpinfo["username"] = "cubeckerlab@gmail.com";
+    //Password redacted for VCS purposes
+    $smtpinfo["password"] = "";
+    $headers = array('From'=>$from, 'To'=>$to, 'Subject'=>$subject);
+    $smtp = &Mail::factory('smtp', $smtpinfo);
+    $mail=$smtp->send($to,$headers,$body);
+    if(PEAR::isError($mail))
+    {
+        echo("<p>" . $mail->getMessage() . "</p>");
+    }
+    else
+    {
+        echo("<p>Message successfully sent!</p>");
+    }
+?>
