8385c94a32ff7483883d69e2fe4de76252d85c8f
aaronrcooper <mr.aaroncooper@gmail.com>
2018-03-05 10:59:05 -0500

    Updated Admin Page
    Merge conflict resulted in the admin page not being merged.

--- pages/admin.php
+++ pages/admin.php
@@ -1,34 +1,7 @@
 <?php
 $title = "Admin Page - Clarion Athletics Site";
 include("../includes/navbar.php"); ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
 
-
-    <title>Clarion University Athletics</title>
-
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="../css/bootstrap.css">
-    <link rel="stylesheet" href="../css/custom.css">
-    <!-- Custom styles for this template -->
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-
-</head>
-
-<body>
-
-<?php
-    $title = "Admin - Clarion Athletics";
-    include("../includes/navbar.php"); ?>
-
-<div class="container">
->>>>>>> Quote_File_Upload
 
     <div class="company-template">
         <h1>Admin Page</h1>
@@ -51,7 +24,6 @@ include("../includes/navbar.php"); ?>
             <div class = "col-6 text-left pl-3"><input name = "userFile" type = "file"/> </div>
             <input type = "submit" value = "Upload Text File" class = "btn-primary ml-auto mr-auto mt-3"/>
         </form>
->>>>>>> Quote_File_Upload
     </div>
 
 <?php
