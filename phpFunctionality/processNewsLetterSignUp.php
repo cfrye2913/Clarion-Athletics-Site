9d3a7bd14ef911bd76ce47047012959d9ec5c118
cfrye2913 <32493117+cfrye2913@users.noreply.github.com>
2018-03-06 13:38:01 -0500

    Added email redundancy checking
    Emails can now only be added to the csv file once

--- phpFunctionality/processNewsLetterSignUp.php
+++ phpFunctionality/processNewsLetterSignUp.php
@@ -11,6 +11,8 @@
 <h1>News Letter Sign Up</h1>
 
 <?php
+    //index of the email within the email csv file
+    $EMAIL_INDEX = 2;
     //Grabs fields from text boxes
     $firstName = $_POST["firstName"];
     $lastName = $_POST["lastName"];
@@ -18,8 +20,7 @@
     $sport = $_POST["sport"];
     $validEmail = true; //will be set to false if the email is invalid
     //open the csv email file
-    $emailfile = fopen("../resourceFiles/email.csv", "a");
-
+    $emailfile = fopen("../resourceFiles/email.csv", "a+");
     //validate the email address, if valid, add to file,
     //if invalid, display an error message
     if(filter_var($email, FILTER_VALIDATE_EMAIL) === false)
@@ -30,11 +31,27 @@
         $validEmail = false;
     }
 
-    //Writes email,name, and sport to email file.
-    $data = array($lastName, $firstName, $email, $sport);
-    fputcsv($emailfile, $data);
-    //force sport string to lower case to display in message to user
-    $sport = strtolower($sport);
-    //display message to user
-    echo  "Welcome, $firstName $lastName! We will try to send you newsletters relevant to $sport.";
+    //get an array of all of the current emails in the csv file
+    //if the email entered is already in the array, display an error
+    //message to the user
+    $currentEmails = array();
+    while($row = fgetcsv($emailfile))
+    {
+       $currentEmails[] = $row[$EMAIL_INDEX];
+    }
+
+    if(in_array($email, $currentEmails) && $validEmail === true)
+    {
+        echo "This email is already signed up to receive our news letter. Please try again";
+        $validEmail = false;
+    }
+    if($validEmail == true) {
+        //Writes email,name, and sport to email file.
+        $data = array($lastName, $firstName, $email, $sport);
+        fputcsv($emailfile, $data);
+        //force sport string to lower case to display in message to user
+        $sport = strtolower($sport);
+        //display message to user
+        echo "Welcome, $firstName $lastName! We will try to send you newsletters relevant to $sport.";
+    }
 ?>
