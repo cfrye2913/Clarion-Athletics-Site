6cd670740fb18e739b35d6ed1e2b2df37b20abfe
aaronrcooper <mr.aaroncooper@gmail.com>
2018-03-29 13:03:29 -0400

    Created mySQL setup file
    Created setup file which contains member and sport table creations.

--- Model/setup.sql
+++ Model/setup.sql
@@ -1,1 +1,15 @@
-CREATE TABLE 
+CREATE TABLE `member` (
+  `member_id`     INT AUTO_INCREMENT,
+  `fname`         VARCHAR (30),
+  `lname`         VARCHAR (30),
+  `signupdate`    DATE,
+  `sport`         TINYINT(32),
+  `email`         VARCHAR(30),
+  CONSTRAINT `member_pk` PRIMARY KEY (`member_id`)
+);
+
+CREATE TABLE `sport` (
+  `sport_num`     INT AUTO_INCREMENT,
+  `sport_name`    VARCHAR(30),
+  CONSTRAINT `sport_pk` PRIMARY KEY (`sport_num`)
+);
