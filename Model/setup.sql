82a687bbdfa551fc5fc1471b2afb5b0922c4bddb
cfrye2913 <32493117+cfrye2913@users.noreply.github.com>
2018-03-29 13:33:24 -0400

    Fixed DDL

--- Model/setup.sql
+++ Model/setup.sql
@@ -1,15 +1,22 @@
+CREATE TABLE `sports` (
+  `sport_num`     INT AUTO_INCREMENT,
+  `sport_name`    VARCHAR(30),
+  CONSTRAINT `sport_pk` PRIMARY KEY (`sport_num`)
+);
+
 CREATE TABLE `member` (
   `member_id`     INT AUTO_INCREMENT,
   `fname`         VARCHAR (30),
   `lname`         VARCHAR (30),
   `signupdate`    DATE,
-  `sport`         TINYINT(32),
+  `sport`         INT,
   `email`         VARCHAR(30),
-  CONSTRAINT `member_pk` PRIMARY KEY (`member_id`)
+   CONSTRAINT `member_sport_fk` FOREIGN KEY(`sport`) REFERENCES `sports`(`sport_num`),
+   CONSTRAINT `member_pk` PRIMARY KEY (`member_id`)
 );
 
-CREATE TABLE `sport` (
-  `sport_num`     INT AUTO_INCREMENT,
-  `sport_name`    VARCHAR(30),
-  CONSTRAINT `sport_pk` PRIMARY KEY (`sport_num`)
+CREATE TABLE `image` (
+  `image_id`      INT AUTO_INCREMENT,
+  `image_path`    TEXT,
+  CONSTRAINT `image_pk` PRIMARY KEY (image_id)
 );
