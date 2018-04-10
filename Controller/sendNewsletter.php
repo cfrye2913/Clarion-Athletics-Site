8fbff9d2e012a7650dd348cc451affd6cf4b1419
aaronrcooper <mr.aaroncooper@gmail.com>
2018-04-10 15:54:32 -0400

    Reduced redundant files
    Eliminated redundancies by deleting redundant files.

--- Controller/sendNewsletter.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $emailPath = "../resourceFiles/email.csv";
-    $emailFile = fopen($emailPath, "r");
-    //Gets the email field from the CSV file
-    while(($newsletterInfo = fgetcsv($emailFile)) !== FALSE)
-    {
-        $tempEmail = $newsletterInfo[2];
-        include("automated_email.php");
-    }
