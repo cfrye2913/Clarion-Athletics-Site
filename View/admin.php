66b9d517bd14a8d2aaa14a8234f931e50b2c5f26
cfrye2913 <32493117+cfrye2913@users.noreply.github.com>
2018-04-09 20:32:42 -0400

    Changed Admin page look

--- View/admin.php
+++ View/admin.php
@@ -4,14 +4,29 @@
     website without editing the source code of the site-->
 <?php
 $title = "Admin Page - Clarion Athletics Site";
-include("includes/navbar.php"); ?>
+include("./includes/navbar.php"); ?>
 
 
-    <div class="company-template">
+<div class="company-template">
+    <h1>Admin Page</h1>
 
-            <h1>Admin Page</h1>
-
-            <h3 class = "mt-3">Add an image</h3>
+    <!-- Tabs for login and sign up -->
+    <ul class="nav nav-tabs col-6 mr-auto ml-auto mt-6 text-center" id="myTab" role="tablist">
+        <li class="nav-item">
+            <a class="nav-link active" id="login-tab" data-toggle="tab" href="#images" role="tab" aria-controls="home" aria-selected="true">Carousel Images</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" id="signup-tab" data-toggle="tab" href="#quotes" role="tab" aria-controls="profile" aria-selected="false">Quotes</a>
+        </li>
+        <li class = "nav-item">
+            <a class="nav-link" id="signup-tab" data-toggle="tab" href="#newsletter" role="tab" aria-controls="profile" aria-selected="false">Newsletter</a>
+        </li>
+    </ul>
+    <!-- Tab content selectable through pills -->
+    <div class = "tab-content col-10 mx-auto">
+        <!-- Carousel image -->
+        <div class="tab-pane active fade show" id="images" role="tabpanel" aria-labelledby="login-tab" aria-expanded="true">
+            <h3 class = "mt-3">Carousel Images</h3>
             <!--Form to upload images to be added to the carousel-->
             <form enctype="multipart/form-data"
                   action = "../index.php?action=add_image"
@@ -22,12 +37,11 @@ include("includes/navbar.php"); ?>
                 <input type = "submit" value = "Upload Image" class = "btn-primary ml-auto mr-auto mt-3 mb-3"/>
             </form>
             <p class="text-danger">For best quality, images should be 1719 x 967 pixels</p>
-
             <h4>Current Images</h4>
             <!--Show all of the image file names that are currently in the directory-->
             <?php
             //Image Path
-            $imageDirName = "Images/CarouselImages/";
+            $imageDirName = "./Images/CarouselImages/";
             //Get all images from the directory and store them in an array
             $imageArray = scandir($imageDirName);
             //loop through the array and display all of the image names
@@ -40,35 +54,40 @@ include("includes/navbar.php"); ?>
                 }
             }
             ?>
-
-
-        <h3 class = "mt-3">Add a quote file</h3>
-        <!--Form to upload a text file of quotes-->
-        <form enctype="multipart/form-data"
-              action = "../index.php?action=add_quote_file"
-              method = "post"
-              class = "row">
-            <div class = "col-6 text-right">Select a new quotes file:</div>
-            <div class = "col-6 text-left pl-3"><input name = "userFile" type = "file"/> </div>
-            <input type = "submit" value = "Upload Text File" class = "btn-primary ml-auto mr-auto mt-3"/>
-        </form>
-
-        <h3 class = "mt-3">Add a news letter</h3>
-        <!--Form to upload an html file for the automated email
-             and a form to send the email-->
-        <form enctype="multipart/form-data"
-              action = "../index.php?action=upload_newsletter"
-              method = "post"
-              class = "row">
-            <div class = "col-6 text-right">Select a newsletter file (HTML format):</div>
-            <div class = "col-6 text-left pl-3"><input name = "userFile" type = "file"/> </div>
-            <input type = "submit" value = "Upload Newsletter" class = "btn-primary ml-auto mr-auto mt-3 mb-3"/>
-        </form>
-        <form action="../Controller/sendNewsletter.php" method="get">
-            <input type="submit" value="Send newsletter" class = "btn-danger">
-        </form>
+        </div>
+        <!-- News Letters -->
+        <div id = "quotes" class = "tab-pane fade" role = "tabpanel" aria-labelledby="signup-tab">
+            <h3 class = "mt-3">Add a quote file</h3>
+            <!--Form to upload a text file of quotes-->
+            <form enctype="multipart/form-data"
+                  action = "./index.php?action=add_quote_file"
+                  method = "post"
+                  class = "row">
+                <div class = "col-6 text-right">Select a new quotes file:</div>
+                <div class = "col-6 text-left pl-3"><input name = "userFile" type = "file"/> </div>
+                <input type = "submit" value = "Upload Text File" class = "btn-primary ml-auto mr-auto mt-3"/>
+            </form>
+        </div>
+        <div id = "newsletter" class = "tab-pane fade" role = "tabpanel" aria-labelledby="signup-tab">
+            <h3 class = "mt-3">Add a news letter</h3>
+            <!--Form to upload an html file for the automated email
+                 and a form to send the email-->
+            <form enctype="multipart/form-data"
+                  action = "../index.php?action=upload_newsletter"
+                  method = "post"
+                  class = "row">
+                <div class = "col-6 text-right">Select a newsletter file (HTML format):</div>
+                <div class = "col-6 text-left pl-3"><input name = "userFile" type = "file"/> </div>
+                <input type = "submit" value = "Upload Newsletter" class = "btn-primary ml-auto mr-auto mt-3 mb-3"/>
+            </form>
+            <form action="../Controller/sendNewsletter.php" method="get">
+                <input type="submit" value="Send newsletter" class = "btn-danger">
+            </form>
+        </div>
     </div>
 
+</div>
+
 <?php
-    include("includes/footer.php");
+include("./includes/footer.php");
 ?>
