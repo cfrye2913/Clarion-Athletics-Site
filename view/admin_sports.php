6074d372f13afba234e8b9962f6546ccba01f7a7
cfrye2913 <32493117+cfrye2913@users.noreply.github.com>
2018-04-29 21:20:47 -0400

    Added sport deleting

--- view/admin_sports.php
+++ view/admin_sports.php
@@ -17,18 +17,39 @@ include("./includes/navbar.php");
     require('./includes/navbar.php');?>
 </head>
 <body>
-    <div id = "sports" class = "jumbotron">
-            <h3 class = "mt-3">Add a sport</h3>
-            <form enctype="multipart/form-data"
-                  action = "index.php?action=add_sport"
-                  method = "post"
-                  class = "row">
-                <div class = "form-row mx-auto">
-                    Sport Name: <input class = "form-control" type = "text" name = "sportName" required />
-                    <input class = "form-control" type = "submit" value = "Add Sport"/>
-                </div>
-            </form>
-    </div>
+<div id = "sports" class = "jumbotron">
+    <h3 class = "mt-3">Add a sport</h3>
+    <form enctype="multipart/form-data"
+          action = "index.php?action=add_sport"
+          method = "post"
+          class = "row">
+        <div class = "form-row mx-auto">
+            Sport Name: <input class = "form-control" type = "text" name = "sportName" required />
+            <input class = "form-control" type = "submit" value = "Add Sport"/>
+        </div>
+    </form>
+    <table class = "mx-auto table table-hover">
+        <thead>
+        <tr>
+            <td><b>Sport</b></td>
+            <td>&nbsp</td>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        $sportList = getSports();
+        if(!empty($sportList)){
+            foreach ($sportList as $sport){
+                echo '<tr>';
+                echo '<td>' . $sport->sportsName . '</td>';
+                echo '<td><a class = "btn btn-primary" href = "index.php?action=remove_sport&id=' . $sport->sportsNum . '"> Delete</a>';
+                echo '</tr>';
+            }
+        }
+        ?>
+        </tbody>
+    </table>
+</div>
 <?php
 include("./includes/footer.php");
 ?>
